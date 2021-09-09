33b9c2100b51e9f561c679d9b922c61c37cb1e35
Imalshi123 <it19133546@my.sliit.lk>
2021-09-09 20:04:35 +0530

    Sprint1-Push

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -6,19 +6,97 @@ let Reservation = require('../models/reservation')
 
 module.exports = function () {
 
+     //Pawani
+    router.post('/make_reservation', function (req, res) {
+        let ReservationData = new Reservation(req.body);
+        ReservationData.save()
+            .then(Reservation => {
+                var data = {
+                    Status: "Sucess",
+                    Message: "Reservation Created Sucessfully"
+                }
+                res.status(201).send(data);
+            }).catch(err => {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            });
+
+    })
+
+    //Pawani
+    router.post('/RemoveUserReservation', function (req, res) {
+        try {
+            console.log(req.body)
+            Reservation.findByIdAndRemove({ _id: req.body.id }, function (err, todo) {
+                if (!err) {
+                    var data = {
+                        Status: "Sucess",
+                        Message: "Reservation Deleted"
+                    }
+                    res.status(200).send(data);
+                } else {
+                    var data = {
+                        Status: "Fail",
+                        Message: "Unexpected Error PLease Contact System Admin"
+                    }
+                    res.status(200).send(data);
+                }
+            });
+
+        } catch {
+            var data = {
+                Status: "Fail",
+                Message: "Unexpected Error PLease Contact System Admin"
+            }
+            res.status(200).send(data);
+
+        }
+
+    })
 
 
-    //Dulanji 
+    //Pawani
+    router.put('/updateReservation', function (req, res) {
+        try {
+            Reservation.updateOne({ _id: req.body.id }, { User_Name: req.body.User_Name, Room_ID: req.body.Room_ID, From_Date: req.body.From_Date, To_Date: req.body.To_Date,Price:req.body.Price,Status:req.body.Status }, function (err, docs) {
+                if (!err) {
+                    var data = {
+                        Status: "Sucess",
+                        Message: "Reservation Data Updated"
+                    }
+                    res.status(200).send(data);
+                } else {
+                    var data = {
+                        Status: "Fail",
+                        Message: "Unexpected Error PLease Contact System Admin"
+                    }
+                    res.status(200).send(data);
+                }
+            })
+        } catch {
+            var data = {
+                Status: "Fail",
+                Message: "Unexpected Error PLease Contact System Admin"
+            }
+            res.status(200).send(data);
 
-    router.get('/get_allReservations', function (req, res) {
-       
+        }
+    })
+
+    //Pawani
+
+    router.post('/get_all_Reservations_byUserID', function (req, res) {
+       console.log(req.body)
         Reservation.find(function (err, data) {
-           
+            var filtered = _.where(data, { User_Name: req.body.User_Name });
             if (!err) {
                 var data = {
                     Status: "Sucess",
                     Message: "Retrived All user Reservations",
-                    data :data
+                    data :filtered
                 }
                 res.status(200).send(data);
             } else {
@@ -30,5 +108,8 @@ module.exports = function () {
             }
         })
     })
+
+
+    
     return router;
 }
