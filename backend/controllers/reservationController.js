1c6f5a5bd1fc2c5584fd297954bbc85b6cfa375c
pawani99z <88001716+pawani99z@users.noreply.github.com>
2021-09-30 10:29:26 -0230

    Final Merged Push

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -137,6 +137,59 @@ module.exports = function () {
      })
 
 
-   
+    //Dulanji 
+
+    router.get('/get_allReservations', function (req, res) {
+       
+        Reservation.find(function (err, data) {
+           
+            if (!err) {
+                var data = {
+                    Status: "Sucess",
+                    Message: "Retrived All user Reservations",
+                    data :data
+                }
+                res.status(200).send(data);
+            } else {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            }
+        })
+    })
+
+    //Dulanji
+
+    router.post('/RemoveReservation', function (req, res) {
+        console.log("Test")
+        try {
+            Reservation.updateOne({ _id: req.body.id }, { User_Name: req.body.User_Name, Room_ID: req.body.Room_ID, From_Date: req.body.From_Date, To_Date: req.body.To_Date,Price:req.body.Price,Status:req.body.Status }, function (err, docs) {
+                if (!err) {
+                    console.log("Test2")
+                    var data = {
+                        Status: "Sucess",
+                        Message: "Reservation Data Updated"
+                    }
+                    res.status(200).send(data);
+                } else {
+                    console.log("Test3")
+                    var data = {
+                        Status: "Fail",
+                        Message: "Unexpected Error PLease Contact System Admin"
+                    }
+                    res.status(200).send(data);
+                }
+            })
+        } catch {
+            var data = {
+                Status: "Fail",
+                Message: "Unexpected Error PLease Contact System Admin"
+            }
+            res.status(200).send(data);
+
+        }
+    })
     return router;
 }
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -101,7 +101,95 @@ module.exports = function () {
         })
     })
 
-   
+    //Imalshi
+
+    router.post('/get_UserData', function (req, res) {
+        console.log(req.body)
+        User.find(function (err, data) {
+            if (!err) {
+                var filtered = _.where(data, { Email: req.body.id });
+                var TotalNumberOfUser = Object.keys(filtered).length;
+                console.log(filtered)
+                if (TotalNumberOfUser == 0) {
+                    var data = {
+                        Status: "Fail",
+                        Message: "Invalid Email"
+                    }
+                    res.status(200).send(data);
+                } else {
+                    console.log(filtered)
+                    var data = {
+                        Status: "Sucess",
+                        Message: "user Data Retrived",
+                        data: filtered
+                    }
+                    res.status(200).send(data);
+                }
+            } else {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            }
+        })
+    })
+
+    //Imalshi
+
+    router.post('/updateUser', function (req, res) {
+        try {
+            User.updateOne({ Email: req.body.Email }, { Full_Name: req.body.Full_Name, Age: req.body.Age, Phone: req.body.Phone, Sex: req.body.sex }, function (err, docs) {
+                if (!err) {
+                    var data = {
+                        Status: "Sucess",
+                        Message: "User Data Updated"
+                    }
+                    res.status(200).send(data);
+                } else {
+                    var data = {
+                        Status: "Fail",
+                        Message: "Unexpected Error PLease Contact System Admin"
+                    }
+                    res.status(200).send(data);
+                }
+            })
+        } catch {
+            var data = {
+                Status: "Fail",
+                Message: "Unexpected Error PLease Contact System Admin"
+            }
+            res.status(200).send(data);
+
+        }
+    })
+
+
+    //Dulanji
+
+    router.get('/get_all_Users', function (req, res) {
+        console.log(req.body)
+        User.find(function (err, data) {
+            if (!err) {
+
+
+
+                var data = {
+                    Status: "Sucess",
+                    Message: "user Data Retrived",
+                    data: data
+                }
+                res.status(200).send(data);
+
+            } else {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            }
+        })
+    })
 
 
     //pawani
@@ -150,7 +238,48 @@ module.exports = function () {
 
     })
 
-    
+    //Imlashi
+    router.post('/AddRefund', function (req, res) {
+        let RF = new Refund(req.body);
+        RF.img="https://assets.website-files.com/5f50dfefde2d2df9368da112/5f6b1251e944e9c21ed2cdec_The-Digital-Business-Owner%E2%80%99s-Guide-To-Refunds_Mesa-de-trabajo-1.png"
+        RF.save()
+            .then(Refund => {
+                var data = {
+                    Status: "Sucess",
+                    Message: "Activity Created Sucessfully"
+                }
+                res.status(201).send(data);
+            }).catch(err => {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            });
+
+    })
+
+
+    //Dulanji
+    router.post('/MakePayment', function (req, res) {
+        let RF = new Payment(req.body);
+        RF.img="https://nmgprod.s3.amazonaws.com/media/files/a9/cf/a9cfd21d1973a93b6b366afa65efad9f/cover_image.jpg.760x400_q85_crop_upscale.jpg"
+        RF.save()
+            .then(Payment => {
+                var data = {
+                    Status: "Sucess",
+                    Message: "Activity Created Sucessfully"
+                }
+                res.status(201).send(data);
+            }).catch(err => {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            });
+
+    })
 
     return router;
 }
