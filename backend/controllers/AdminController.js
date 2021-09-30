70930a20b0a1e042c6e80f166974ba9d8a2378a9
DuliLopez <68705294+DuliLopez@users.noreply.github.com>
2021-09-30 10:25:33 -0230

    Fixes

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -11,6 +11,104 @@ let Refund = require('../models/refund')
 let Payment = require('../models/payment')
 const saltRounds = 10;
 module.exports = function () {
+    
+    //Pawani
+    router.post('/add_activity', function (req, res) {
+        let ac = new Activity(req.body);
+        ac.img="http://ses.ssd6.org/files/2017/12/activities.jpg"
+        ac.save()
+            .then(Activity => {
+                var data = {
+                    Status: "Sucess",
+                    Message: "Activity Created Sucessfully"
+                }
+                res.status(201).send(data);
+            }).catch(err => {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            });
+
+    })
+
+    //Pawani
+    router.post('/getActivityByID', function (req, res) {
+        console.log(req.body)
+        Activity.find(function (err, dataX) {
+
+            if (!err) {
+                var filtered = _.where(dataX, { ActivityName: req.body.id });
+                console.log("HU", filtered)
+                var data = {
+                    Status: "Sucess",
+                    Message: "Retrived All Room Data",
+                    data: filtered
+                }
+                res.status(200).send(data);
+            } else {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            }
+        })
+    })
+
+    //pawani 
+    router.post('/RemoveActivity', function (req, res) {
+        console.log(req.body, "here")
+        try {
+            Activity.findByIdAndRemove({ _id: req.body.id }, function (err, todo) {
+                if (!err) {
+                    var data = {
+                        Status: "Sucess",
+                        Message: "User Deleted"
+                    }
+                    res.status(200).send(data);
+                } else {
+                    var data = {
+                        Status: "Fail",
+                        Message: "Unexpected Error PLease Contact System Admin"
+                    }
+                    res.status(200).send(data);
+                }
+            });
+
+        } catch {
+            var data = {
+                Status: "Fail",
+                Message: "Unexpected Error PLease Contact System Admin"
+            }
+            res.status(200).send(data);
+
+        }
+
+    })
+    //pawani
+    router.get('/getAllFeedBack', function (req, res) {
+
+        FeedBack.find(function (err, dataX) {
+
+            if (!err) {
+                var data = {
+                    Status: "Sucess",
+                    Message: "Retrived All Activitues",
+                    data: dataX
+                }
+                res.status(200).send(dataX);
+            } else {
+                var data = {
+                    Status: "Fail",
+                    Message: "Unexpected Error PLease Contact System Admin"
+                }
+                res.status(200).send(data);
+            }
+        })
+    })
+    
 
     //Dulanji
     router.post('/add_manager', function (req, res) {
